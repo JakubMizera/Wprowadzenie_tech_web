2b5cef70303fa1d1c6a8316348d68982796b03f2
JakubMizera <kubamiz53@gmail.com>
2021-06-09 16:45:49 +0200

    js tries

--- lab7/hamburgerMenu.js
+++ lab7/hamburgerMenu.js
@@ -4,23 +4,36 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
 
-const img1 = document.querySelector("#img1");
-img1.addEventListener("click", () => openLightBox("images/girl.jpg"));
+let photo = "";
 
-function openLightBox(imgSrc) {
-  const lightBox = document.querySelector(".lightBox");
-  const lightBoxImage = document.querySelector("#lightBoxImage");
-  lightBox.classList.add("visible");
-  lightBoxImage.src = imgSrc;
-}
 
 window.onclick = (e) => {
+  console.log(e.target.src);
   console.log(e.target);
+  photo = e.target.src;
+  let photoSrc = e.target;
+  return photoSrc
 };
 
-// img1.addEventListener("click", openLightBox);
+const img1 = document.querySelector("#img1");
+// const anyImg = document.querySelectorAll("img");
+// console.log(anyImg);
+const body = document.querySelector("body");
+const lightBox = document.querySelector(".lightBox");
+const lightBoxImage = document.querySelector("#lightBoxImage");
 
-// function openLightBox() {
-//   const lightBox = document.querySelector(".lightBox");
-//   lightBox.classList.add("visible");
-// }
+// img1.addEventListener("click", () => openLightBox(photo));
+photoSrc.addEventListener("click", () => openLightBox(photo));
+
+function openLightBox(imgSrc) {
+  lightBox.classList.add("visible");
+  lightBoxImage.src = imgSrc;
+  body.style.overflow = "hidden";
+}
+
+lightBox.addEventListener("click", () => closeLightBox());
+
+function closeLightBox() {
+  lightBox.classList.remove("visible");
+  body.style.overflow = "visible";
+}
