254e10692c3eda085a1ce9a15c56031c494986bd
JakubMizera <kubamiz53@gmail.com>
2021-06-09 14:19:01 +0200

    styles html added

--- lab7/hamburgerMenu.js
+++ lab7/hamburgerMenu.js
@@ -1,7 +1,26 @@
 const btn = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
-console.log(btn);
-console.log(nav);
 btn.addEventListener("click", () => {
-    nav.classList.toggle("open");
+  nav.classList.toggle("open");
 });
+
+const img1 = document.querySelector("#img1");
+img1.addEventListener("click", () => openLightBox("images/girl.jpg"));
+
+function openLightBox(imgSrc) {
+  const lightBox = document.querySelector(".lightBox");
+  const lightBoxImage = document.querySelector("#lightBoxImage");
+  lightBox.classList.add("visible");
+  lightBoxImage.src = imgSrc;
+}
+
+window.onclick = (e) => {
+  console.log(e.target);
+};
+
+// img1.addEventListener("click", openLightBox);
+
+// function openLightBox() {
+//   const lightBox = document.querySelector(".lightBox");
+//   lightBox.classList.add("visible");
+// }
