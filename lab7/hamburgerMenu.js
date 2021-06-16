70de08229621d615d82a5179f0c4b59fbc22c334
JakubMizera <kubamiz53@gmail.com>
2021-06-16 13:19:13 +0200

    photo pop up

--- lab7/hamburgerMenu.js
+++ lab7/hamburgerMenu.js
@@ -4,30 +4,26 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
 
-let photo = "";
+// let photo = "";
 
-
-window.onclick = (e) => {
-  console.log(e.target.src);
-  console.log(e.target);
-  photo = e.target.src;
-  let photoSrc = e.target;
-  return photoSrc
-};
+// window.onclick = (e) => {
+//   console.log(e.target.src);
+//   console.log(e.target);
+//   photo = e.target.src;
+//   let photoSrc = e.target;
+//   return photoSrc
+// };
 
 const img1 = document.querySelector("#img1");
-// const anyImg = document.querySelectorAll("img");
-// console.log(anyImg);
 const body = document.querySelector("body");
 const lightBox = document.querySelector(".lightBox");
 const lightBoxImage = document.querySelector("#lightBoxImage");
 
-// img1.addEventListener("click", () => openLightBox(photo));
-photoSrc.addEventListener("click", () => openLightBox(photo));
+img1.addEventListener("click", () => openLightBox("../images/girl.jpg"));
 
 function openLightBox(imgSrc) {
-  lightBox.classList.add("visible");
   lightBoxImage.src = imgSrc;
+  lightBox.classList.add("visible");
   body.style.overflow = "hidden";
 }
 
